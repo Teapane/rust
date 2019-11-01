8a5fde3e8ea1e9644850eafb4228a41bc34783a7
Tyler Long <tyler.long@guildeducation.com>
2019-11-01 13:11:40 -0600

    Add rand crate
    - Add some comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// import input/output from Standard Lib
+use std::io;
+
+// Entry point
 fn main() {
-    println!("Hello, world!");
+    // print line
+    println!("Guess the number!");
+    println!("Please Input your guess.");
+
+    // makes guess variable mutable
+    let mut guess = String::new();
+
+    // Reads users input. Expect is a safety check
+    // &mut _references_ guess va
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // print the users inputed val
+    println!("You guessed: {}", guess);
 }
